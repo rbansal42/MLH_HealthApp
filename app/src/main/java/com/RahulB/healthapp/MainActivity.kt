a128c5d71b15831155dd0ff6a8ea2c20836a8182
Rahul Bansal <rahulbansal.btech20@sushantuniversity.edu.in>
2021-06-29 14:22:49 +0530

    It should be working?

--- app/src/main/java/com/RahulB/healthapp/MainActivity.kt
+++ app/src/main/java/com/RahulB/healthapp/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.RahulB.healthapp
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    var running = false
+    var sensorManager: SensorManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
+
+    override fun onResume() {
+        super.onResume()
+        running = true
+        var stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+        if(stepSensor == null) {
+            Toast.makeText(this, "No Step Counter Sensor!", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        running = false
+        sensorManager?.unregisterListener(this)
+        Toast.makeText(this, "Counter Paused!", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(running) {
+            // Update the UI
+            val steps: TextView = findViewById(R.id.StepCount)
+            if (event != null) {
+                steps.text = event.values[0].toString()
+            }
+        }
+        TODO("Not yet implemented")
+    }
+
 }
